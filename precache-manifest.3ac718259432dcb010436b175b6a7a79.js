060d00f341be7d92a35c54e8e046ee3e96703c76
Alfredo Zimperz <alfredo.zimperz@gmail.com>
2020-08-03 19:45:31 -0300

    Updates

---
+++ precache-manifest.3ac718259432dcb010436b175b6a7a79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cef0429d752b1be5be39ddbb3b0d84ce",
+    "url": "/react-simple-todo/index.html"
+  },
+  {
+    "revision": "71319e41b8866d307d32",
+    "url": "/react-simple-todo/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "95c765c72efbd8d05e28",
+    "url": "/react-simple-todo/static/js/2.bfe1bbb4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-simple-todo/static/js/2.bfe1bbb4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71319e41b8866d307d32",
+    "url": "/react-simple-todo/static/js/main.feefd1fa.chunk.js"
+  },
+  {
+    "revision": "2e5abd417065f9dbbf5c",
+    "url": "/react-simple-todo/static/js/runtime-main.92a474d3.js"
+  }
+]);
